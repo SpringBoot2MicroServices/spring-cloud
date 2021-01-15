f7addf037151904f4c4735369289d9dd7202dd0e
AkuthotaRaghu1202 <akuthotaraghu@gmail.com>
2021-01-16 02:28:23 +0530

    order-service and payment-service route url configured in yml file

--- src/main/java/com/arjavatech/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/arjavatech/cloud/gateway/CloudGatewayApplication.java
@@ -2,12 +2,14 @@ package com.arjavatech.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/arjavatech/cloud/gateway/controller/GatewatTestController.java
@@ -0,0 +1,7 @@
+package com.arjavatech.cloud.gateway.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewatTestController {
+}
